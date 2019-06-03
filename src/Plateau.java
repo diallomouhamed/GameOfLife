074650d9df4aeebe0a143625558be4a4aa8c7c2e
Mouhamed Diallo <Mouhamed.dia2l@yahoo.fr>
2019-06-03 15:52:53 +0200

    Initialisation du projet

---
+++ src/Plateau.java
@@ -0,0 +1,15 @@
+public class Plateau {
+    int taille;
+    int [][] plateau;
+
+    public Plateau(int taille) {
+        this.taille = taille;
+        plateau = new int [taille][taille];
+    }
+
+    public void initialisation() {
+        for(int i = 0; i < taille; i++) {
+
+        }
+    }
+}
